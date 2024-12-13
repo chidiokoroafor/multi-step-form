8b91d7a9884b44c56bc4be48c2827e3eb5a35f97
chidiokoroafor <cokoroafor@healthstation.ng>
2024-12-13 15:40:43 +0100

    fix the  UI

--- src/components/MyTextInput.tsx
+++ src/components/MyTextInput.tsx
@@ -9,7 +9,7 @@ const MyTextInput = ({ label, ...props }:TextInputProp) => {
               <label className="text-MarineBlue" htmlFor={ props.name}>{ label }</label>
             {meta.touched && meta.error ? <p className="text-StrawberryRed text-sm">{`${meta.error}` }</p> : null}
         </div>
-        <input {...field} {...props} className="px-4 py-2 outline-CoolGray rounded-lg border-2 mt-1 border-CoolGray w-full" type="text"  />
+        <input {...field} {...props} className="px-4 py-3 outline-CoolGray rounded-lg border-2 mt-1 border-CoolGray w-full" type="text"  />
     </div>
   )
 }
